95c087927b3cea7f4a82323f048ab145185dce45
JulioFlores <juliobraydifn@gmail.com>
2025-03-07 19:57:09 +0100

    feat: Instalación y configuracuón básica de Webpack, Babel, Sass y Jest

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: './dist',
+    port: 9000
+  },
+  devtool: 'source-map'
+};
